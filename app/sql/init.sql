d2242b4ace29e9a05f488118abb66b625ed97db8
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2025-05-22 19:38:25 +0800

    fix : query sql

--- app/sql/init.sql
+++ app/sql/init.sql
@@ -6,13 +6,13 @@ USE sistem_pencatatan_keuangan;
 -- TABLES category icome
 CREATE TABLE IF NOT EXISTS category_income (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    category VARCHAR(100) NOT NULL,
+    name VARCHAR(50) NOT NULL
 );
 
 -- TABLES category expense
 CREATE TABLE IF NOT EXISTS category_expense (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    category VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL
 );
 
 -- TABLES income
@@ -24,15 +24,14 @@ CREATE TABLE IF NOT EXISTS income (
     date DATE NOT NULL,
     FOREIGN KEY (category_id) REFERENCES category_income(id)
     
-)
+);
 
 -- TABLES expense
-CREATE TABLE IF NOT EXISTS income (
+CREATE TABLE IF NOT EXISTS expense (
     id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
     category_id INT NOT NULL,
     description VARCHAR(100) NOT NULL,
     amount DECIMAL(10, 2) NOT NULL,
     date DATE NOT NULL,
     FOREIGN KEY (category_id) REFERENCES category_expense(id)
-)
-
+);
